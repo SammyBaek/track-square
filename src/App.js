3d866986b9014dc5e93dde0fb6b784540f04b5f4
Sammy Baek <kie827@gmail.com>
2020-05-02 18:20:58 -0400

    implement track history

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
+const TRACK_FREQ = 1000;
+
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -14,11 +16,16 @@ class App extends React.Component {
       count: 0,
       position: null,
       text: "",
+      intervalId: null,
     };
-    this.handleGetLocation = this.handleGetLocation.bind(this);
-    this.getPosition = this.getPosition.bind(this);
+    this.handleGetCurLocation = this.handleGetCurLocation.bind(this);
     this.errorMessage = this.errorMessage.bind(this);
     this.renderTableData = this.renderTableData.bind(this);
+    this.startTracking = this.startTracking.bind(this);
+    this.trackPosition = this.trackPosition.bind(this);
+    this.getLocation = this.getLocation.bind(this);
+    this.stopTracking = this.stopTracking.bind(this);
+    this.handleClearHistory = this.handleClearHistory.bind(this);
 
     this.positionOptions = {
       enableHighAccuracy: true,
@@ -27,13 +34,49 @@ class App extends React.Component {
     };
   }
 
-  getPosition(pos) {
-    console.log(pos);
+  trackPosition(pos) {
+    console.log("track", pos);
     this.setState({
       position: pos,
-      text: pos.coords.latitude + " : " + pos.coords.longitude,
+      text: "",
       locations: [...this.state.locations, pos],
     });
+  }
+
+  startTracking(e) {
+    e.preventDefault();
+    if (this.state.intervalId === null) {
+      const intervalId = setInterval(() => {
+        this.getLocation((pos) => {
+          let lastPos = null;
+          if (this.state.locations.length > 0) {
+            lastPos = this.state.locations[this.state.locations.length - 1];
+          }
+          if (lastPos !== null && lastPos.timestamp === pos.timestamp) {
+            return;
+          }
+          this.setState({
+            position: pos,
+            text: "",
+            locations: [...this.state.locations, pos],
+          });
+        });
+      }, TRACK_FREQ);
+      this.setState({intervalId});
+    }
+  }
+
+  handleClearHistory(e) {
+    e.preventDefault();
+    this.setState({locations: []});
+  }
+
+  stopTracking(e) {
+    e.preventDefault();
+    if (this.state.intervalId !== null) {
+      clearInterval(this.state.intervalId);
+      this.setState({intervalId: null});
+    }
   }
 
   errorMessage(error) {
@@ -58,17 +101,26 @@ class App extends React.Component {
     this.setState({text});
   }
 
-  handleGetLocation(e) {
-    e.preventDefault();
+  getLocation(cb) {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
-        this.getPosition,
+        cb,
         this.errorMessage,
         this.positionOptions
       );
     } else {
       this.setState({text: "unavailable"});
     }
+  }
+
+  handleGetCurLocation(e) {
+    e.preventDefault();
+    this.getLocation((pos) => {
+      this.setState({
+        position: pos,
+        text: "",
+      });
+    });
   }
 
   renderTableData() {
@@ -89,17 +141,46 @@ class App extends React.Component {
 
   render() {
     const date = new Date();
+    const {position, intervalId} = this.state;
+    let text = this.state.text;
+    if (position !== null) {
+      text = `current location: ${position.coords.latitude} : ${position.coords.longitude}`;
+    }
+
+    let trackBtn = null;
+    if (intervalId !== null) {
+      trackBtn = (
+        <Button variant="warning" onClick={this.stopTracking}>
+          Pause Tracking Location
+        </Button>
+      );
+    } else {
+      trackBtn = (
+        <Button variant="primary" onClick={this.startTracking}>
+          Start Tracking Location
+        </Button>
+      );
+    }
+
     return (
       <Container fluid>
         <Row>
           <Col xs={4}>{date.getTime()}</Col>
           <Col xs={4}>
-            <Button variant="primary" onClick={this.handleGetLocation}>
-              Get Location
+            <Button variant="success" onClick={this.handleGetCurLocation}>
+              Get Current Location
             </Button>
           </Col>
           <Col xs={4}>
-            <div>{this.state.text}</div>
+            <div>{text}</div>
+          </Col>
+        </Row>
+        <Row>
+          <Col>{trackBtn}</Col>
+          <Col>
+            <Button variant="danger" onClick={this.handleClearHistory}>
+              Clear Track History
+            </Button>
           </Col>
         </Row>
         <Row>
