d461798c8644116d9ca05ddda06d9e879829f9e7
Sammy Baek <kie827@gmail.com>
2020-05-31 02:17:45 -0400

    fix bugs for auto serve

--- server_setup.sh
+++ server_setup.sh
@@ -14,36 +14,38 @@ installSoftware() {
     sudo apt-get update
     sudo add-apt-repository -y ppa:deadsnakes/ppa
     sudo add-apt-repository -y ppa:certbot/certbot
+    sudo add-apt-repository universe
     sudo apt-get update
     sudo apt-get install software-properties-common python-certbot-nginx build-essential
-    sudo apt-get install nginx python-pip python3-pip nodejs npm gunicorn python3-venv certbot
+    sudo apt-get install nginx python-pip python3-pip nodejs npm python3-venv certbot python3-certbot-nginx
 }
 
 setupNginx() {
     printf '****************************** setup nginx ****************************** \n'
     sudo rm -rf /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
     sudo bash -c 'cat > /etc/nginx/sites-available/track-square.nginx <<EOF
-    server {
-        listen 80;
-        root /home/ubuntu/track-square/build;
-        index index.html;
+server {
+    listen 80;
+    root /home/ubuntu/track-square/build;
+    index index.html;
 
-        location / {
-            try_files $uri $uri/ =404;
-            add_header Cache-Control "no-cache";
-        }
-
-        location /static {
-            expires 1y;
-            add_header Cache-Control "public";
-        }
-
-        location /api {
-            include proxy_params;
-            proxy_pass http://localhost:5000;
-        }
+    location / {
+        try_files $uri $uri/ =404;
+        add_header Cache-Control "no-cache";
     }
-    '
+
+    location /static {
+        expires 1y;
+        add_header Cache-Control "public";
+    }
+
+    location /api {
+        include proxy_params;
+        proxy_pass http://localhost:5000;
+    }
+}
+
+'
 
     sudo ln -s /etc/nginx/sites-available/track-square.nginx /etc/nginx/sites-enabled/track-square.nginx
     sudo systemctl reload nginx
@@ -59,9 +61,8 @@ installRepoDependencies() {
     cd /home/ubuntu/track-square/server
     python3 -m venv venv
     source venv/bin/activate
-    sudo pip3 install -r requirements.txt
-    sudo pip3 install gunicorn
-    sudo pip install gunicorn
+    pip install -r requirements.txt
+    pip install gunicorn
 }
 
 startGunicorn() {
@@ -69,19 +70,20 @@ startGunicorn() {
     cd /home/ubuntu/track-square
 
     sudo bash -c 'cat > /etc/systemd/system/track-square.service <<EOF
-    [Unit]
-    Description=A simple Flask API
-    After=network.target
+[Unit]
+Description=A simple Flask API
+After=network.target
 
-    [Service]
-    User=ubuntu
-    WorkingDirectory=/home/ubuntu/track-square
-    ExecStart=/home/ubuntu/track-square/server/venv/bin/gunicorn -b 127.0.0.1:5000 "server:create_app()"
-    Restart=always
+[Service]
+User=ubuntu
+WorkingDirectory=/home/ubuntu/track-square
+ExecStart=/home/ubuntu/track-square/server/venv/bin/gunicorn -b 127.0.0.1:5000 "server:create_app()"
+Restart=always
 
-    [Install]
-    WantedBy=multi-user.target
-    '
+[Install]
+WantedBy=multi-user.target
+
+'
 
     sudo systemctl daemon-reload
     sudo systemctl start track-square
@@ -94,41 +96,42 @@ enableSSL() {
     sudo rm /etc/nginx/sites-available/track-square.nginx
     sudo rm /etc/nginx/sites-enabled/track-square.nginx
     sudo bash -c 'cat > /etc/nginx/sites-available/track-square.nginx <<EOF
-    server {
-        listen 80;
-        server_name tracksquare.app www.tracksquare.app;
-        location ~ /.well-known {
-            root /home/ubuntu/cert;
-        }
-        location / {
-            return 301 https://$host$request_uri;
-        }
+server {
+    listen 80;
+    server_name tracksquare.app www.tracksquare.app;
+    location ~ /.well-known {
+        root /home/ubuntu/cert;
+    }
+    location / {
+        return 301 https://$host$request_uri;
+    }
+}
+
+server {
+    listen 443 ssl;
+    server_name tracksquare.app www.tracksquare.app;
+    ssl_certificate /etc/letsencrypt/live/tracksquare.app/fullchain.pem;
+    ssl_certificate_key /etc/letsencrypt/live/tracksquare.app/privkey.pem;
+    root /home/ubuntu/track-square/build;
+    index index.html;
+
+    location / {
+        try_files $uri $uri/ =404;
+        add_header Cache-Control "no-cache";
     }
 
-    server {
-        listen 443 ssl;
-        server_name tracksquare.app www.tracksquare.app;
-        ssl_certificate /etc/letsencrypt/live/tracksquare.app/fullchain.pem;
-        ssl_certificate_key /etc/letsencrypt/live/tracksquare.app/privkey.pem;
-        root /home/ubuntu/track-square/build;
-        index index.html;
+    location /static {
+        expires 1y;
+        add_header Cache-Control "public";
+    }
 
-        location / {
-            try_files $uri $uri/ =404;
-            add_header Cache-Control "no-cache";
-        }
+    location /api {
+        include proxy_params;
+        proxy_pass http://localhost:5000;
+    }
+}
 
-        location /static {
-            expires 1y;
-            add_header Cache-Control "public";
-        }
-
-        location /api {
-            include proxy_params;
-            proxy_pass http://localhost:5000;
-        }
-    }    
-    '
+'
 
     sudo ln -s /etc/nginx/sites-available/track-square.nginx /etc/nginx/sites-enabled/track-square.nginx
 
@@ -144,7 +147,7 @@ run() {
     setupNginx
     installRepoDependencies
     startGunicorn
-    enableSSL
+    # enableSSL
 }
 
 run
